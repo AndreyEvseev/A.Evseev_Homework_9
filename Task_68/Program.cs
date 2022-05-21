981094369cad75747a392d43678b68a5325bbfd7
Andrey Evseev <a-evseev@yandex.ru>
2022-05-21 18:16:53 +0300

    код задачи 68

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -1,58 +1,63 @@
 ﻿// Задача 68. Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 //            Даны два неотрицательных числа m и n.
 
-void TitleInputErrorMessage()
-{   Console.WriteLine("Уважаемый пользователь, вы ошиблись при вводе! ");
+void TitleInputErrorMessage() 
+{
+    Console.WriteLine("Уважаемый пользователь, вы ошиблись при вводе! ");
 }
 
-void NumberInputErrorMessage (string userNumber, string definitionNumber)
-{   TitleInputErrorMessage();
+void NumberInputErrorMessage (string userNumber, string definitionNumber) 
+{
+    TitleInputErrorMessage();
     Console.Write($"Введённое Вами значение: {userNumber},");
     Console.WriteLine($" не является {definitionNumber} числом.");
 }
 
-int CheckNaturalNumbers (string messageForUser)
-{   int number = 0, i = 0;
+int CheckIntegerPositiveNumbers (string messageForUser) 
+{
+    int number = 0, i = 0;
     string userNumber = string.Empty;
     string definitionNumber = "натуральным";
     bool success = false;
     while(i == 0) 
-    {   Console.Write(messageForUser);
+    {
+        Console.Write(messageForUser);
         userNumber = Console.ReadLine();
         success = int.TryParse(userNumber, out number);
-        if(success)
-        {   number = int.Parse(userNumber);
-            if(number > 0) i = 1;
+        if(success) 
+        {
+            number = int.Parse(userNumber);
+            if(number >= 0) i = 1;
             else NumberInputErrorMessage(userNumber, definitionNumber); 
         }
         else 
-        {   NumberInputErrorMessage(userNumber, definitionNumber);
+        {
+            NumberInputErrorMessage(userNumber, definitionNumber);
         } 
-        Console.WriteLine();
     }
     return number;
 }
 
-int InputIntegerNumber(string text1, string text2, string text3)
-{   string messegeForUser = string.Empty;
+int InputIntegerPositiveNumber(string text1, string text2, string text3) 
+{
+    string messegeForUser = string.Empty;
     messegeForUser = $"Введите {text1} {text2} {text3}: ";
-    int result = CheckNaturalNumbers(messegeForUser); 
+    int result = CheckIntegerPositiveNumbers(messegeForUser); 
     return result; 
 }
 
-int AckermanRec(int m, int n)
-{   int a = 0;
-    if(m == 0) return a + n + 1;
-    else if(m != 0 && n == 0) return a + AckermanRec(m - 1, n);
-        else if(m != 0 && n != 0) return AckermanRec(m - 1, AckermanRec(m, n - 1));
-            else return AckermanRec(m, n);
-    }
+int AckermanFunc(int m, int n) 
+{
+    if(m == 0) return n + 1;
+    if(n == 0) return AckermanFunc(m - 1, 1); 
+    return AckermanFunc(m - 1, AckermanFunc(m, n - 1));
+}
 
-Console.WriteLine("Программа вычисления функции Аккермана: A(m, n).");
+Console.WriteLine("Вычисление функции Аккермана.");
 int m, n;
 string text1 = string.Empty, text2 = string.Empty, text3 = string.Empty;
-text1 = "натуральное"; text2 = "число"; text3 = "m";
-m = InputIntegerNumber(text1, text2, text3);
+text1 = "целое положительное"; text2 = "число"; text3 = "m";
+m = InputIntegerPositiveNumber(text1, text2, text3);
 text3 = "n";
-n = InputIntegerNumber(text1, text2, text3);
-Console.WriteLine($"Значение функции Аккермана: A({m}, {n}): {AckermanRec(m, n)}.");
+n = InputIntegerPositiveNumber(text1, text2, text3);
+Console.WriteLine($"Значение функции Аккермана: A({m}, {n}): {AckermanFunc(m, n)}.");
